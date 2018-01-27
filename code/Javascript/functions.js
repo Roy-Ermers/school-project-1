1c320ada029bd197328d9fb9f2b5384763f98873
Roy Ermers <dangergamesnl@gmail.com>
2018-01-27 14:06:38 +0100

    added menus

--- code/Javascript/functions.js
+++ code/Javascript/functions.js
@@ -1,3 +1,19 @@
 function downloadFile(location) {
 	window.location = location;
 }
+function showMenu() {
+	var navbar = document.querySelector("nav>div");
+	navbar.classList.toggle("show");
+}
+function hideMenu() {
+	var navbar = document.querySelector("nav>div");
+	navbar.classList.toggle("show");
+}
+/**
+ * toggles the submenus in 
+ * @param {Element} element 
+ */
+function toggleSubMenu(element) {
+	console.log(element);
+	element.classList.toggle("show");
+}
